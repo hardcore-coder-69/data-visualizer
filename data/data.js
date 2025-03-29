9dbfe1a3d5b836d88a8bcad31280f61d5e160938
Sunit Mishra <sunitm@contata.in>
2025-03-29 17:17:50 +0530

    initial commit

---
+++ data/data.js
@@ -0,0 +1,54 @@
+window.bgImage = "./assets/clouds.png";
+
+window.data = [
+    {
+        image: "./assets/steve-balmer.jpg",
+        heading: "<h2>Steve Ballmer</h2> <h2 style='background: #fff; color: #000;'>$136.2 billion</h2>",
+        text: "<p>Source of income:</p><p>Former CEO of Microsoft</p>"
+    },
+    {
+        image: "./assets/amancio.jpg",
+        heading: "<h2>Amancio Ortega</h2> <h2 style='background: #fff; color: #000;'>$124 billion</h2>",
+        text: "<p>Source of income:</p><p>Founder of Inditex</p>"
+    },
+    {
+        image: "./assets/brin.jpg",
+        heading: "<h2>Sergey Brin</h2> <h2 style='background: #fff; color: #000;'>$140.7 billion</h2>",
+        text: "<p>Source of income:</p><p>Co-founder of Google</p>"
+    },
+    {
+        image: "./assets/larry.jpg",
+        heading: "<h2>Larry Page</h2> <h2 style='background: #fff; color: #000;'>$149.4 billion</h2>",
+        text: "<p>Source of income:</p><p>Co-founder of Google</p>"
+    },
+    {
+        image: "./assets/warren.jpg",
+        heading: "<h2>Warren Buffett</h2> <h2 style='background: #fff; color: #000;'>$160.7 billion</h2>",
+        text: "<p>Source of income:</p><p>CEO of Berkshire Hathaway</p>"
+    },
+    {
+        image: "./assets/bernard.jpg",
+        heading: "<h2>Bernard Arnault</h2> <h2 style='background: #fff; color: #000;'>$176.7 billion</h2>",
+        text: "<p>Source of income:</p><p>Chairman and CEO of LVMH</p>"
+    },
+    {
+        image: "./assets/ellison.png",
+        heading: "<h2>Larry Ellison</h2> <h2 style='background: #fff; color: #000;'>$169.3 billion</h2>",
+        text: "<p>Source of income:</p><p>The co-founder and chairman of Oracle</p>"
+    },
+    {
+        image: "./assets/jeff.jpg",
+        heading: "<h2>Jeff Bezos</h2> <h2 style='background: #fff; color: #000;'>$227 billion</h2>",
+        text: "<p>Source of income:</p><p>Founder of Amazon.com</p>"
+    },
+    {
+        image: "./assets/zuck.png",
+        heading: "<h2>Mark Zuckerberg</h2> <h2 style='background: #fff; color: #000;'>$231 billion</h2>",
+        text: "<p>Source of income:</p><p>CEO of Meta</p>"
+    },
+    {
+        image: "./assets/elon.png",
+        heading: "<h2>Elon Musk</h2> <h2 style='background: #fff; color: #000;'>$359.5 billion</h2>",
+        text: "<p>Source of income:</p><p>Tesla</p><p>SpaceX</p><p>X</p><p>Neuralink</p><p>Others...</p>"
+    }
+]
---
+++ script.js
@@ -0,0 +1,84 @@
+
+drawUI();
+function drawUI() {
+    let leftPos = 800;
+    let topPos = 100;
+    let scale = 1.2;
+    if (window.outerWidth <= 600) {
+        leftPos = 500;
+        topPos = 0;
+        scale = 2;
+    }
+
+    const containerEl = document.getElementById('container');
+
+    if (bgImage) {
+        const bgEl = document.getElementById('background-image');
+        bgEl.setAttribute('src', bgImage);
+        bgEl.style.display = 'block';
+    }
+
+    console.log(data);
+
+    let uiString = '';
+    for (let i = 0; i < data.length; i++) {
+        let item = data[i];
+        let itemUI = `
+            <div class="data-item" id="item-${i}" style="top: ${topPos}px; left: ${leftPos * (i + 1)}px; transform: scale(${scale});">
+                <div class="image-box">
+                    <img class="image" src="${item.image}" />
+                </div>
+
+                <div class="cuboid">
+                    <div class="face front">
+                        <div class="bar">
+                            <div class="heading">${item.heading}</div>
+                            <div class="text">${item.text}</div>
+                        </div>
+                    </div>
+                    <div class="face back"></div>
+                    <div class="face right">
+                        <div class="bar">
+                            <div class="heading">${item.heading}</div>
+                            <div class="text">${item.text}</div>
+                        </div>
+                    </div>
+                    <div class="face left"></div>
+                    <div class="face top"></div>
+                </div>
+            </div>
+        `;
+
+        uiString += itemUI;
+    }
+    containerEl.innerHTML = uiString;
+
+    setTimeout(() => {
+        startAnimation();
+    }, 3000);
+}
+
+function startAnimation() {
+    const containerEl = document.getElementById('container');
+    containerEl.style.transition = 'all 120s linear';
+    containerEl.style.transform = `translateX(-${800 * data.length}px)`;
+
+    scaleDownHanlder();
+}
+
+function scaleDownHanlder() {
+    let scaleOn = 500;
+    if(window.outerWidth <= 600) {
+        scaleOn = 300;
+    }
+    
+    const items = Array.from(document.getElementsByClassName("data-item"));
+    setInterval(() => {
+        items.forEach(item => {
+            if (item.getBoundingClientRect().x < scaleOn) {
+                item.classList.add('scale-down');
+                item.style.transform = '';
+            }
+        })
+    }, 100);
+}
