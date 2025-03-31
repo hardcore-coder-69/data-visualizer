97cef241610ecdd99244a995f931458cebdfcbe7
Sunit Mishra <sunitm@contata.in>
2025-03-31 21:54:42 +0530

    new

--- data/data.js
+++ data/data.js
@@ -1,64 +1,64 @@
-window.bgImage = "./assets/bg.png";
+window.bgImage = "";
 
 window.data = [
     {
-        image: "./assets/delphi.png",
-        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%;'>10</span></h2> <h2 style='background: #fff; color: #000;'>Delphi Pascal</h2>",
-        text: "<p>Used primarily for developing desktop, mobile, web, and console applications.</p>",
-        background: "#DA242C"
+        image: "./assets/sein.png",
+        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%;'>10</span></h2> <h2 style='background: #fff; color: #000;'>Seinfeld <br> (1989–1998)</h2>",
+        text: "<p>A “show about nothing” that changed sitcoms forever.</p>",
+        // background: "#DA242C"
     },
     {
-        image: "./assets/vb.png",
-        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>9</span></h2> <h2 style='background: #fff; color: #000;'>Visual Basic</h2>",
-        text: "<p>Event-driven programming language</p>",
-        background: "#004D8D"
+        image: "./assets/twilight.png",
+        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>9</span></h2> <h2 style='background: #fff; color: #000;'>The Twilight Zone <br> (1959–1964)</h2>",
+        text: "<p>The original mind-bending anthology series.</p>",
+        // background: "#004D8D"
     },
     {
-        image: "./assets/sql.png",
-        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>8</span></h2> <h2 style='background: #fff; color: #000;'>SQL</h2>",
-        text: "<p>Used for managing and manipulating relational databases</p>",
-        background: "#0B5975"
+        image: "./assets/mash.jpg",
+        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>8</span></h2> <h2 style='background: #fff; color: #000;'>MAS*H <br> (1972–1983)</h2>",
+        text: "<p>A legendary war comedy-drama set during the Korean War.</p>",
+        // background: "#0B5975"
     },
     {
-        image: "./assets/go.png",
-        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>7</span></h2> <h2 style='background: #fff; color: #000;'>GO</h2>",
-        text: "<p>Used particularly in cloud computing and distributed systems</p>",
-        background: "#00AED9"
+        image: "./assets/stranger.jpg",
+        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>7</span></h2> <h2 style='background: #fff; color: #000;'>Stranger Things <br> (2016–present)</h2>",
+        text: "<p>Introduced a new generation to the magic of supernatural storytelling.</p>",
+        // background: "#00AED9"
     },
     {
-        image: "./assets/JavaScript.png",
-        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>6</span></h2> <h2 style='background: #fff; color: #000;'>JavaScript</h2>",
-        text: "<p>A lightweight, interpreted language primarily known for enhancing interactivity in web browsers.</p>",
-        background: "#F0DC4E"
+        image: "./assets/office.jpg",
+        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>6</span></h2> <h2 style='background: #fff; color: #000;'>The Office (U.S.) <br> (2005–2013)</h2>",
+        text: "<p>Known for its awkward humor</p>",
+        // background: "#F0DC4E"
     },
     {
-        image: "./assets/csharp.png",
-        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>5</span></h2> <h2 style='background: #fff; color: #000;'>C#</h2>",
-        text: "<p>Used for creating a range of applications within the .NET framework</p>",
-        background: "#A27ADD"
+        image: "./assets/sopra.jpg",
+        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>5</span></h2> <h2 style='background: #fff; color: #000;'>The Sopranos <br> (1999–2007)</h2>",
+        text: "<p>The ultimate gangster drama following mob boss Tony Soprano.</p>",
+        // background: "#A27ADD"
     },
     {
-        image: "./assets/c.png",
-        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>4</span></h2> <h2 style='background: #fff; color: #000;'>C</h2>",
-        text: "<p>A foundational programming language</p>",
-        background: "#649AD2"
+        image: "./assets/simpsons.jpg",
+        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>4</span></h2> <h2 style='background: #fff; color: #000;'>The Simpsons <br> (1989–present)</h2>",
+        text: "<p>The longest-running animated sitcom.</p>",
+        // background: "#649AD2"
     },
     {
-        image: "./assets/java.jpg",
-        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>3</span></h2> <h2 style='background: #fff; color: #000;'>Java</h2>",
-        text: "<p>A class-based, object-oriented language</p>",
-        background: "#02758D"
+        image: "./assets/friends.jpg",
+        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>3</span></h2> <h2 style='background: #fff; color: #000;'>Friends <br> (1994–2004)</h2>",
+        text: "<p>Timeless humor, iconic catchphrases, and cultural impact.</p>",
+        // background: "#02758D"
     },
     {
-        image: "./assets/cpp.png",
-        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>2</span></h2> <h2 style='background: #fff; color: #000;'>CPP</h2>",
-        text: "<p>An extension of the C language</p>",
-        background: "#659BD3"
+        image: "./assets/got.jpg",
+        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>2</span></h2> <h2 style='background: #fff; color: #000;'>Game of Thrones <br> (2011–2019)</h2>",
+        text: "<p>Epic fantasy drama based on George R.R. Martin’s books.</p>",
+        // background: "#659BD3"
     },
     {
-        image: "./assets/python.jpg",
-        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>1</span></h2> <h2 style='background: #fff; color: #000;'>Python</h2>",
-        text: "<p>A versatile, high-level programming language</p>",
-        background: "#0176BC"
+        image: "./assets/br.png",
+        heading: "<h2><span style='font-size: 40px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>1</span></h2> <h2 style='background: #fff; color: #000;'>Breaking Bad <br> (2008–2013)</h2>",
+        text: "<p>Gritty crime drama about a high school teacher-turned-drug lord.</p>",
+        // background: "#0176BC"
     }
 ]
