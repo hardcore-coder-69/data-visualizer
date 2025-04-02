5ba31d3cd6e8307de0d14b2afe6fab27485499da
Sunit Mishra <sunitm@contata.in>
2025-04-02 17:26:34 +0530

    new

--- data/data.js
+++ data/data.js
@@ -1,65 +1,66 @@
 window.bgImage = "";
-// Source: United Nations Population Division
+// Top 10 Largest Economies by GDP (2025 Projections)
+// Source: International Monetary Fund (IMF)
 
 window.data = [
     {
         image: "./assets/10.webp",
-        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%;'>10</span></h2> <h2 style='background: #fff; color: #000;'>Mexico</h2>",
-        text: "<p style='font-weight: 500; font-size: 22px;'>130 Million people</p>",
-        background: "linear-gradient(to right, #086D4D, #fff, #D0182D);"
+        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%;'>10</span></h2> <h2 style='background: #fff; color: #000;'>Canada</h2>",
+        text: "<p style='font-weight: 500; font-size: 22px;'>$1.9 Trillion</p>",
+        background: "linear-gradient(to right, #D52B1E, #fff, #D52B1E);"
     },
     {
-        image: "./assets/9.webp",
-        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>9</span></h2> <h2 style='background: #fff; color: #000;'>Russia</h2>",
-        text: "<p style='font-weight: 500; font-size: 22px;'>145 Million people</p>",
-        background: "linear-gradient(to right, #fff, #0038A4, #D52A1D);"
+        image: "./assets/9.png",
+        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>9</span></h2> <h2 style='background: #fff; color: #000;'>Italy</h2>",
+        text: "<p style='font-weight: 500; font-size: 22px;'>$2.0 Trillion</p>",
+        background: "linear-gradient(to right, #009344, #FFF, #CF2734);"
     },
     {
-        image: "./assets/8.webp",
-        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>8</span></h2> <h2 style='background: #fff; color: #000;'>Bangladesh</h2>",
-        text: "<p style='font-weight: 500; font-size: 22px;'>170 Million people</p>",
-        background: "linear-gradient(to right, #006B50, #EE2E44, #006B50);"
-    },
-    {
-        image: "./assets/7.png",
-        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>7</span></h2> <h2 style='background: #fff; color: #000;'>Brazil</h2>",
-        text: "<p style='font-weight: 500; font-size: 22px;'>215 Million people</p>",
+        image: "./assets/8.png",
+        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>8</span></h2> <h2 style='background: #fff; color: #000;'>Brazil</h2>",
+        text: "<p style='font-weight: 500; font-size: 22px;'>$2.5 Trillion</p>",
         background: "linear-gradient(to right, #00953E, #FFCC00, #2C2182, #00953E);"
     },
     {
-        image: "./assets/6.png",
-        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>6</span></h2> <h2 style='background: #fff; color: #000;'>Nigeria</h2>",
-        text: "<p style='font-weight: 500; font-size: 22px;'>230 Million people</p>",
-        background: "linear-gradient(to right, #008850, #fff, #008850);"
-    },
-    {
-        image: "./assets/5.png",
-        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>5</span></h2> <h2 style='background: #fff; color: #000;'>Pakistan</h2>",
-        text: "<p style='font-weight: 500; font-size: 22px;'>240 Million people</p>",
-        background: "linear-gradient(to right, #FFFFFF, #003F15);"
-    },
-    {
-        image: "./assets/4.webp",
-        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>4</span></h2> <h2 style='background: #fff; color: #000;'>Indonesia</h2>",
-        text: "<p style='font-weight: 500; font-size: 22px;'>280 Million people</p>",
-        background: "linear-gradient(to right, #FF0000, #fff);"
-    },
-    {
-        image: "./assets/3.webp",
-        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>3</span></h2> <h2 style='background: #fff; color: #000;'>United States</h2>",
-        text: "<p style='font-weight: 500; font-size: 22px;'>335 Million people</p>",
+        image: "./assets/7.png",
+        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>7</span></h2> <h2 style='background: #fff; color: #000;'>France</h2>",
+        text: "<p style='font-weight: 500; font-size: 22px;'>$2.9 Trillion</p>",
         background: "linear-gradient(to right, #002768, #fff, #BE0B31);"
+    },
+    {
+        image: "./assets/6.webp",
+        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>6</span></h2> <h2 style='background: #fff; color: #000;'>United Kingdom</h2>",
+        text: "<p style='font-weight: 500; font-size: 22px;'>$3.1 Trillion</p>",
+        background: "linear-gradient(to right, #fff, #C8102E, #012169, #fff);"
+    },
+    {
+        image: "./assets/5.svg",
+        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>5</span></h2> <h2 style='background: #fff; color: #000;'>India</h2>",
+        text: "<p style='font-weight: 500; font-size: 22px;'>$3.7 Trillion</p>",
+        background: "linear-gradient(to right, #FF6820, #fff, #046A38);"
+    },
+    {
+        image: "./assets/4.png",
+        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>4</span></h2> <h2 style='background: #fff; color: #000;'>Germany</h2>",
+        text: "<p style='font-weight: 500; font-size: 22px;'>$4.9 Trillion</p>",
+        background: "linear-gradient(to right, #000, #FF0000, #D0B638);"
+    },
+    {
+        image: "./assets/3.png",
+        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>3</span></h2> <h2 style='background: #fff; color: #000;'>Japan</h2>",
+        text: "<p style='font-weight: 500; font-size: 22px;'>$5.1 Trillion</p>",
+        background: "linear-gradient(to right, #fff, #BD0029, #fff);"
     },
     {
         image: "./assets/2.webp",
         heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>2</span></h2> <h2 style='background: #fff; color: #000;'>China</h2>",
-        text: "<p style='font-weight: 500; font-size: 22px;'>1.41 Billion people</p>",
+        text: "<p style='font-weight: 500; font-size: 22px;'>$18.8 Trillion</p>",
         background: "linear-gradient(to right, #BE0B31, #FFFF00, #BE0B31);"
     },
     {
-        image: "./assets/1.svg",
-        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>1</span></h2> <h2 style='background: #fff; color: #000;'>India</h2>",
-        text: "<p style='font-weight: 500; font-size: 22px;'>1.43 Billion people</p>",
-        background: "linear-gradient(to right, #FF6820, #fff, #046A38);"
+        image: "./assets/1.webp",
+        heading: "<h2><span style='font-size: 32px; background: #fff; color: #000; padding: 5px; border-radius: 50%; padding-left: 16px; padding-right: 16px;'>1</span></h2> <h2 style='background: #fff; color: #000;'>United States</h2>",
+        text: "<p style='font-weight: 500; font-size: 22px;'>$25.3 Trillion</p>",
+        background: "linear-gradient(to right, #BE0B31, #fff, #002768, #BE0B31);"
     }
 ]
